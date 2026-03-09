ce6429fc10e360431405c8a878948782c2ee9305
sgxman <jonny.olsson@xlent.se>
2026-03-09 21:37:08 +0100

    Add new image: ChatGPT Image Mar 9, 2026, 08_34_51 PM.png to images_original directory

--- src/data/cases.ts
+++ src/data/cases.ts
@@ -4,19 +4,22 @@ export const cases = [
   {
     id: '1',
     title: 'Bröllop med kräftskiva',
-    type: 'Sommarstugefest',
+    type: 'Bröllop',
     guests: 50,
     image: createInspirationImageSet('insp_kraftbrollop_001', { defaultWidth: 800 }).src,
     imageSrcSet: createInspirationImageSet('insp_kraftbrollop_001', { defaultWidth: 800 }).srcSet,
     imageSizes: '(max-width: 640px) 100vw, (max-width: 1280px) 50vw, 33vw',
-    description: 'En härlig sommarlunch med lantlig charm och vackra detaljer i naturliga toner.',
-    fullDescription: 'En minnesvärd sommarfest med 50 gäster i en vacker trädgård. Vi skapade en lantlig och elegant atmosfär med naturliga material och varma toner.',
-    products: ['Vintage porslin', 'Kristallglas', 'Linnedukar', 'Blommor i krukor'],
+    description: 'Temabröllop i härlig sommarstugemiljö.',
+    fullDescription: 'Genom att jobba med den naturliga miljön i en sommarstuga skapades ett riktigt minnesvärt bröllop med kräftskivetema. Gästerna välkomnades med fördrink på verandan och en stor dynlåda fick agera bar under kvällen. Det stora tältet smyckades med blad och blommor från skogen intill och kröntes med kristallkrona. Eftersom det var självplock av kräftor på buffen var dukningen avskalad, men stilfull. I det enkla bor det vackra, som Ernst säger.',
+    products: ['Tyllgardiner', 'Porslin', 'Glas', 'Linnedukar', 'Dekorationer', 'Stolsöverdrag', 'Ljusslingor'],
     images: [
-      { name: 'insp_kraftbrollop_001', imageText: 'Här har olle städat och gjort fint' },
-      "insp_kraftbrollop_002",
-      'insp_kraftbrollop_004'
-    ],
+      { name: 'insp_kraftbrollop_001', imageText: 'Här har Jonny städat och gjort fint' },
+      'insp_kraftbrollop_004',
+      'insp_kraftbrollop_005',
+      'insp_kraftbrollop_006',
+      'insp_kraftbrollop_007',
+      'insp_kraftbrollop_008',
+      'insp_kraftbrollop_011'],
     season: 'Sommar'
   },
   {
@@ -27,9 +30,9 @@ export const cases = [
     image: createInspirationImageSet('insp_student_002', { defaultWidth: 800 }).src,
     imageSrcSet: createInspirationImageSet('insp_student_002', { defaultWidth: 800 }).srcSet,
     imageSizes: '(max-width: 640px) 100vw, (max-width: 1280px) 50vw, 33vw',
-    description: 'Ett drömliknande bröllop med gulddetaljer och romantisk belysning.',
-    fullDescription: 'Ett vackert bröllop med 80 gäster där elegans och romantik stod i fokus. Gulddetaljer och ljusslingor skapade en magisk atmosfär.',
-    products: ['Champagneglas', 'Guldbestick', 'Ljusslingor', 'Bordsdekorationer'],
+    description: 'Studentmottagning med snittar.',
+    fullDescription: 'En smidig studentmottagning i trädgården med vänner och familj. Långbänkar vid borden gav en informell känsla och de färgglada dekorationerna förstärkte festkänslan. För att förenkla logistiken fanns ett bord för dryck, ett för presenter och samt buffebord vid husväggen utanför tältet. På det sättet stördes inte festen av omdukning mellan plockmat och kaffe och tårta. ',
+    products: ['Champagneglas', 'Porslin', 'Serveringsbrädor', 'Dekoration'],
     images: [
       'insp_student_002',
       'insp_student_001'
@@ -38,15 +41,15 @@ export const cases = [
   },
   {
     id: '3',
-    title: 'Höstmiddag hos vänner',
-    type: 'Middagsfest',
-    guests: 20,
+    title: 'Hälsingebröllop',
+    type: 'Bröllop',
+    guests: 40,
     image: createInspirationImageSet('hostmiddag_vanner_001', { defaultWidth: 800 }).src,
     imageSrcSet: createInspirationImageSet('hostmiddag_vanner_001', { defaultWidth: 800 }).srcSet,
     imageSizes: '(max-width: 640px) 100vw, (max-width: 1280px) 50vw, 33vw',
-    description: 'En mysig höstmiddag med varma färger och stämningsfull belysning.',
-    fullDescription: 'En intim höstmiddag med 20 gäster där vi skapade en varm och mysig känsla med höstens färger och ljus.',
-    products: ['Stengodsporslin', 'Glaslyktor', 'Höstdekoration', 'Vinkällare'],
+    description: 'Utomhusbröllop i Hälsingland.',
+    fullDescription: 'En vacker bröllopsfest med 40 gäster på en Hälsingsk innergård. Efter mingel, spel och lekar utomhus följde en trerätters middag i logen. Baren byggdes upp i ett närligggande rum och ljusslingor förstärkte den magiska känslan både inne och ute.',
+    products: ['Porslin', 'Glas', 'Barbåge', 'Linnedukar', 'Stolsöverdrag', 'Tyllgardiner', 'Dekorationer', 'Ljusslingor'],   
     images: [
       'hostmiddag_vanner_001',
       'hostmiddag_vanner_002'
